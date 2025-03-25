bead181442618777e53a99df3fbb9f7b162941f8
lathusanthurairajah@gmail.com <lathusanthurairajah@gmail.com>
2025-03-25 11:17:23 +0000

    DB main & mirror changes

--- src/main/java/com/pos_main/Config/FailoverRoutingDataSource.java
+++ src/main/java/com/pos_main/Config/FailoverRoutingDataSource.java
@@ -1,33 +1,33 @@
-package com.pos_main.Config;
-
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import javax.sql.DataSource;
-import java.util.Map;
-
-/**
- * Title: FailoverRoutingDataSource.java. Company: www.codearson.com |
- * Copyright: Copyright (C) 2025.
- * @author Lathusan Thurairajah.
- * @date Mar 25, 2025.
- * @version 1.0
- **/
-
-public class FailoverRoutingDataSource extends AbstractRoutingDataSource {
-	
-	public FailoverRoutingDataSource(DataSource mainDataSource, DataSource mirrorDataSource) {
-		setTargetDataSources(Map.of("main", mainDataSource, "mirror", mirrorDataSource));
-		setDefaultTargetDataSource(mainDataSource);
-		afterPropertiesSet();
-	}
-
-	@Override
-	protected Object determineCurrentLookupKey() {
-		try {
-			// Check if main database is available
-			return "main";
-		} catch (Exception e) {
-			// If main fails, switch to mirror
-			return "mirror";
-		}
-	}
-}
+//package com.pos_main.Config;
+//
+//import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+//import javax.sql.DataSource;
+//import java.util.Map;
+//
+///**
+// * Title: FailoverRoutingDataSource.java. Company: www.codearson.com |
+// * Copyright: Copyright (C) 2025.
+// * @author Lathusan Thurairajah.
+// * @date Mar 25, 2025.
+// * @version 1.0
+// **/
+//
+//public class FailoverRoutingDataSource extends AbstractRoutingDataSource {
+//	
+//	public FailoverRoutingDataSource(DataSource mainDataSource, DataSource mirrorDataSource) {
+//		setTargetDataSources(Map.of("main", mainDataSource, "mirror", mirrorDataSource));
+//		setDefaultTargetDataSource(mainDataSource);
+//		afterPropertiesSet();
+//	}
+//
+//	@Override
+//	protected Object determineCurrentLookupKey() {
+//		try {
+//			// Check if main database is available
+//			return "main";
+//		} catch (Exception e) {
+//			// If main fails, switch to mirror
+//			return "mirror";
+//		}
+//	}
+//}
--- src/main/java/com/pos_main/Config/JpaConfig.java
+++ src/main/java/com/pos_main/Config/JpaConfig.java
@@ -1,50 +1,40 @@
-package com.pos_main.Config;
-
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.orm.jpa.JpaTransactionManager;
-
-import java.util.Properties;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
-/**
- * Title: JpaConfig.java. Company: www.codearson.com | Copyright: Copyright (C)
- * 2025.
- * 
- * @author Lathusan Thurairajah.
- * @date Mar 25, 2025.
- * @version 1.0
- **/
-
-@Configuration
-public class JpaConfig {
-
-	@Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("com.pos_main");
-        em.setPersistenceUnitName("pos_main_db");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        em.setJpaProperties(properties);
-
-        return em;
-    }
-
-	@Bean
-	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-		return new JpaTransactionManager(entityManagerFactory);
-	}
-}
+//package com.pos_main.Config;
+//
+//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.transaction.PlatformTransactionManager;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//
+//import javax.persistence.EntityManagerFactory;
+//import javax.sql.DataSource;
+//
+///**
+// * Title: JpaConfig.java. Company: www.codearson.com | Copyright: Copyright (C)
+// * 2025.
+// * 
+// * @author Lathusan Thurairajah.
+// * @date Mar 25, 2025.
+// * @version 1.0
+// **/
+//
+//@Configuration
+//public class JpaConfig {
+//
+//	@Bean
+//	public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
+//		return new EntityManagerFactoryBuilder(null, null, null);
+//	}
+//
+//	@Bean
+//	public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
+//			DataSource dataSource) {
+//		return builder.dataSource(dataSource).packages("com.pos_main").persistenceUnit("pos_main_db").build();
+//	}
+//
+//	@Bean
+//	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+//		return new JpaTransactionManager(entityManagerFactory);
+//	}
+//}
