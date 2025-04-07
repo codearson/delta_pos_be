a86385858ebb65422aab514eb7eb71292a7cb8d6
Thuvaragan Sritharan <srithuva2005@gmail.com>
2025-04-07 23:46:10 +0530

    getAllPAge created for Purchase List

--- src/main/java/com/pos_main/Dao/PurchaseListDao.java
+++ src/main/java/com/pos_main/Dao/PurchaseListDao.java
@@ -1,8 +1,10 @@
 package com.pos_main.Dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.pos_main.Domain.PurchaseList;
+import com.pos_main.Dto.PaginatedResponseDto;
 import com.pos_main.Dto.PurchaseListDto;
 
 public interface PurchaseListDao extends BaseDao<PurchaseList> {
@@ -10,6 +12,8 @@ public interface PurchaseListDao extends BaseDao<PurchaseList> {
 	PurchaseList savePurchaseList(PurchaseList purchaseList);
 	
 	List<PurchaseListDto> getAll();
+	
+	PaginatedResponseDto getAllPagePurchaseList(int pageNumber, int pageSize, Map<String, String> searchParams);
 	
 	boolean deleteAll();
 	
--- src/main/java/com/pos_main/DaoImpl/PurchaseListDaoImpl.java
+++ src/main/java/com/pos_main/DaoImpl/PurchaseListDaoImpl.java
@@ -2,6 +2,8 @@ package com.pos_main.DaoImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
@@ -9,11 +11,15 @@ import javax.transaction.Transactional;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.pos_main.Dao.PurchaseListDao;
+import com.pos_main.Domain.Customer;
 import com.pos_main.Domain.PurchaseList;
+import com.pos_main.Dto.PaginatedResponseDto;
 import com.pos_main.Dto.PurchaseListDto;
+import com.pos_main.Service.Utils.HttpReqRespUtils;
 import com.pos_main.Transformer.PurchaseListTransformer;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +30,9 @@ public class PurchaseListDaoImpl extends BaseDaoImpl<PurchaseList> implements Pu
 
     @Autowired
     private PurchaseListTransformer purchaseListTransformer;
+    
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private EntityManager entityManager; // Replace SessionFactory with EntityManager
@@ -54,6 +63,34 @@ public class PurchaseListDaoImpl extends BaseDaoImpl<PurchaseList> implements Pu
 			}
 		}
 		return purchaseListDtoList;
+	}
+    
+	@Override
+	@Transactional
+	public PaginatedResponseDto getAllPagePurchaseList(int pageNumber, int pageSize, Map<String, String> searchParams) {
+		log.info("PurchaseListDaoImpl.getAll()invoked");
+		PaginatedResponseDto paginatedResponseDto = null;
+		List<PurchaseList> allPurchaseListList = null;
+		int recordCount = 0;
+		String countString = "SELECT COUNT(*) FROM purchase_list";
+		int count = jdbcTemplate.queryForObject(countString, Integer.class);
+
+		if (pageSize == 0) {
+			pageSize = count;
+		}
+
+		Criteria criteria = getCurrentSession().createCriteria(PurchaseList.class, "purchaseList");
+		criteria.setFirstResult((pageNumber - 1) * pageSize);
+		criteria.setMaxResults(pageSize);
+		allPurchaseListList = criteria.list();
+		recordCount = allPurchaseListList.size();
+		if (allPurchaseListList != null && !allPurchaseListList.isEmpty()) {
+			paginatedResponseDto = HttpReqRespUtils.paginatedResponseMapper(allPurchaseListList, pageNumber, pageSize, count);
+			paginatedResponseDto.setPayload(allPurchaseListList.stream().map(Invoice -> {
+				return purchaseListTransformer.transform(Invoice);
+			}).collect(Collectors.toList()));
+		}
+		return paginatedResponseDto;
 	}
     
     @Override
