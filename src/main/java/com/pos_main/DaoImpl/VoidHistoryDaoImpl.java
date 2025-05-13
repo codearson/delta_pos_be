41facd11eec396bcb5fd8fcd0cb4d166e09101f5
Kuganesan Bavithragithan <skbavi61@gmail.com>
2025-05-13 20:06:06 +0530

    Update VoidHistoryDaoImpl.java

--- src/main/java/com/pos_main/DaoImpl/VoidHistoryDaoImpl.java
+++ src/main/java/com/pos_main/DaoImpl/VoidHistoryDaoImpl.java
@@ -80,7 +80,8 @@ public class VoidHistoryDaoImpl extends BaseDaoImpl<VoidHistory> implements Void
 		}
 
 		Criteria criteria = getCurrentSession().createCriteria(VoidHistory.class, "voidHistory");
-				
+
+		criteria.addOrder(Order.desc("id"));
 		criteria.setFirstResult((pageNumber - 1) * pageSize);
 		criteria.setMaxResults(pageSize);
 		allVoidHistoryList = criteria.list();
@@ -111,7 +112,8 @@ public class VoidHistoryDaoImpl extends BaseDaoImpl<VoidHistory> implements Void
 		Criteria criteria = getCurrentSession().createCriteria(VoidHistory.class, "voidHistory");
 		criteria.createAlias("User", "user");
 		criteria.add(Restrictions.eq("user.id", userId));
-				
+
+		criteria.addOrder(Order.desc("id"));
 		criteria.setFirstResult((pageNumber - 1) * pageSize);
 		criteria.setMaxResults(pageSize);
 		allVoidHistoryList = criteria.list();
@@ -141,6 +143,7 @@ public class VoidHistoryDaoImpl extends BaseDaoImpl<VoidHistory> implements Void
 
 		Criteria criteria = getCurrentSession().createCriteria(VoidHistory.class, "voidHistory");
 		criteria.add(Restrictions.sqlRestriction("DATE(this_.date_time) = ?", date, org.hibernate.type.StringType.INSTANCE));
+		criteria.addOrder(Order.desc("id"));
 		criteria.setFirstResult((pageNumber - 1) * pageSize);
 		criteria.setMaxResults(pageSize);
 		allVoidHistoryList = criteria.list();
